c398b410683d31fc6dc20c6fe5393d1248a83244
Michael Davis <mcarsondavis@gmail.com>
2022-01-15 08:46:29 -0600

    make locals.scm queries more granular

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,6 +1,8 @@
 ; Scopes
 (function_body) @local.scope
 
+(case_clause) @local.scope
+
 ; Definitions
 (let pattern: (identifier) @local.definition)
 (function_parameter name: (identifier) @local.definition)
@@ -10,4 +12,16 @@
 (record_pattern_argument pattern: (identifier) @local.definition)
 
 ; References
-(identifier) @local.reference
+(field_access
+  record: (identifier) @local.reference
+  field: (identifier) @local.reference)
+
+(function_call
+  function: (identifier) @local.reference)
+
+(argument
+  value: (identifier) @local.reference)
+
+(binary_expression (identifier) @local.reference)
+(case_subjects (identifier) @local.reference)
+(tuple (identifier) @local.reference)
