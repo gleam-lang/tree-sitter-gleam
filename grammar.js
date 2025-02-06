1f2cd195b1588c371077d4559b5780c4419e93f2
Alex Manning <alex41290@gmail.com>
2025-02-05 19:57:38 -0500

    fix(functions): remove function body alias and use block

--- grammar.js
+++ grammar.js
@@ -255,13 +255,7 @@ module.exports = grammar({
           field("name", $.identifier),
           field("parameters", $.function_parameters),
           optional(seq("->", field("return_type", $._type))),
-          optional(
-            seq(
-              "{",
-              optional(field("body", alias($._statement_seq, $.function_body))),
-              "}"
-            )
-          )
+          optional(field("body", $.block))
         )
       ),
     function_parameters: ($) =>
@@ -406,9 +400,7 @@ module.exports = grammar({
           alias($.anonymous_function_parameters, $.function_parameters)
         ),
         optional(seq("->", field("return_type", $._type))),
-        "{",
-        field("body", alias($._statement_seq, $.function_body)),
-        "}"
+        field("body", $.block)
       ),
     anonymous_function_parameters: ($) =>
       seq(
