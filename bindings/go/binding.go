1627dc5101e63bf19717c540a56df5ef20b1fc7a
Michael Davis <mcarsondavis@gmail.com>
2026-04-20 10:31:31 -0400

    Include src dir in Go bindings
    This is the same as https://github.com/airbus-cert/tree-sitter-powershell/pull/43,
    it should fix a compilation error when using this grammar via the go
    bindings.

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -1,6 +1,6 @@
 package tree_sitter_gleam
 
-// #cgo CFLAGS: -std=c11 -fPIC
+// #cgo CFLAGS: -std=c11 -fPIC -I${SRCDIR}/../../src
 // #include "../../src/parser.c"
 // #if __has_include("../../src/scanner.c")
 // #include "../../src/scanner.c"
